1c9825812448a19bc194b7762980468b9bc72d49
whatnirja <nirjadabhi34@gmail.com>
2026-08-20 13:05:15 -0400

    feat: define CNN architecture, loss function, and optimizer

--- train_digit_model.py
+++ train_digit_model.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+import torch.nn as nn
 from torchvision import transforms
 
 transform = transforms.ToTensor()
@@ -22,3 +23,32 @@ test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=F
 
 print("Training samples:", len(train_dataset))
 print("Test samples:", len(test_dataset))
+
+class Net(torch.nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
+    self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+
+    self.pool = nn.MaxPool2d(2, 2)
+
+    self.fc1 = nn.Linear(1568, 128)
+    self.fc2 = nn.Linear(128, 10)
+
+  def forward(self, x):
+    x = torch.relu(self.conv1(x))
+    x = self.pool(x)
+
+    x = torch.relu(self.conv2(x))
+    x = self.pool(x)
+
+    x = x.view(x.size(0), -1)
+
+    x = torch.relu(self.fc1(x))
+    x = self.fc2(x)
+
+    return x
+
+model = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
